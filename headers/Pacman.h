2b59a8732ca22f4dc97ad9aae08d6ba4963c4b13
levon <levon>
2025-01-29 20:27:41 +0400

    Ghosts are rendered and now working on ghost movement

--- headers/Pacman.h
+++ headers/Pacman.h
@@ -9,16 +9,16 @@ class Pacman {
 public:
   Pacman(float pac_x, float pac_y, const std::string texture_path);
   void draw_packman(sf::RenderWindow& window);
-  void animation_render(const int anim_speed, int pac_ord, int pac_dir);
+  void animation_render(const int anim_speed);
   void move_to_dir(Move_Direction dir, std::vector<sf::RectangleShape>& all_rect);
   void eating_pallet(Map& map,  std::vector<sf::RectangleShape>& all_pallets);
   void increase_frm_speed();
   const sf::Sprite get_pacman() const;
 
 private:
-  int pacman_ord;
-  int pacman_dir; 
-  int frame_speed;
+  int m_pacman_ord;
+  int m_pacman_dir; 
+  int m_frame_speed;
 
   float m_pacman_x;
   float m_pacman_y;
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <fstream>
-#include "headers/GameEngine.h"
-
-int main() {
-    GameEngine game;
-    game.game_start();
- }
